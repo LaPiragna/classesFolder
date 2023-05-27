443289155fe55498d9f2b79cbf5174a628cd16c7
Gonzalo Varela <88951520+LaPiragna@users.noreply.github.com>
2023-05-26 22:15:42 -0300

    yey

--- 05-23/clase 18-05-23/enum/src/Main.java
+++ 05-23/clase 18-05-23/enum/src/Main.java
@@ -4,22 +4,8 @@ import netscape.javascript.JSObject;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        /*
         Equipo miEquipo = Equipo.BOKE;
         Enums.WeekDay lunes = Enums.WeekDay.LUNES;
         System.out.println(lunes.getNombre());
-         */
-
-        Json json = new Json();
-
     }
 }
-//los get pueden lanzar excepciones.
-// clave inexistente
-// solicitud erronea en tipo de dato con respecto a una key
-//
-//los get, pueden solicitar un jsonObject o un json Array, el primero con
-//una clave, y para lo segundo con el indice.
-
-//la cosa seria despues haber leido esto -> 1) sí. 2) no.
-// -        -       -       -                       -> answer:
